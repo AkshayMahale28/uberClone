8d87146d011f9de5cc086ea8cf061d6270c41dcf
Akshay Mahale <akshaymahale2830011@gmail.com>
2025-07-11 10:53:26 +0530

    user UI Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,7 @@ import UserLogout from "./pages/UserLogout";
 import CaptainHome from "./pages/CaptainHome";
 import CaptainProtectWrapper from "./pages/CaptainProtectWrapper";
 import CaptainLogout from "./pages/CaptainLogout";
+import Riding from "./pages/Riding";
 
 const App = () => {
   return (
@@ -55,6 +56,7 @@ const App = () => {
         {/* <Route path="/captain-home" element={<CaptainHome />} /> */}
 
         <Route path="/login" element={<UserLogin />} />
+        <Route path="/riding" element={<Riding />} />
         <Route path="/signup" element={<UserSignup />} />
         <Route path="/captain-signup" element={<CaptainSignup />} />
         <Route path="/captain-login" element={<CaptainLogin />} />
